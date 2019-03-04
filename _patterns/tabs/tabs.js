459f4bf93ede48d7d9597d0eb2dd7ba9d10b18d8
arobillard <robi0653@algonquinlive.com>
2019-03-04 12:40:53 -0500

    Make some tweaks to the tab design

--- _patterns/tabs/tabs.js
+++ _patterns/tabs/tabs.js
@@ -1,9 +1,14 @@
 $('.section-tab-btn').on('click', function (e) {
   var id = $(this).attr('href');
+  var page = $(this).attr('data-page');
 
   e.preventDefault();
   $('.section-tab-btns li').removeClass('section-tab-selected');
   $(this).parent().addClass('section-tab-selected');
+  $('.section-tab-btns').removeClass('tab-page-1');
+  $('.section-tab-btns').removeClass('tab-page-2');
+  $('.section-tab-btns').removeClass('tab-page-3');
+  $('.section-tab-btns').addClass(page);
   $('.section-tab-page').removeClass('section-tab-page-active');
   $(id).addClass('section-tab-page-active');
 });
